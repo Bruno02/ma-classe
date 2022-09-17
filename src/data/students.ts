d596b81d4b7b8496f9e1a226ca3546a711588116
Bruno Fima <bruno.fima@aktome.fr>
2022-09-17 10:46:53 +0200

    students list data

--- src/data/students.ts
+++ src/data/students.ts
@@ -2,21 +2,33 @@ import { IStudent } from 'src/reducers/students';
 
 export const STUDENTS: IStudent[] = [
   {
-    id: '1',
+    id: '1000',
     firstname: 'Bob',
     lastname: 'Dylan',
     address: 'NY',
   },
   {
-    id: '2',
+    id: '1001',
     firstname: 'Al',
     lastname: 'Pacino',
     address: 'NY',
   },
   {
-    id: '3',
+    id: '1002',
     firstname: 'Robert',
     lastname: 'Deniro',
+    address: 'NY',
+  },
+  {
+    id: '1003',
+    firstname: 'Jimi',
+    lastname: 'Hendrix',
+    address: 'NY',
+  },
+  {
+    id: '1004',
+    firstname: 'Jeff',
+    lastname: 'Bridges',
     address: 'NY',
   },
 ];
